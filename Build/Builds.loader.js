6637f1d3a91c5c93edd42d8eb717e33388a78402
Franco <francorr240@gmail.com>
2025-01-23 22:54:25 -0300

    Added a bunch of stuff

--- Build/Builds.loader.js
+++ Build/Builds.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 61222228,
+    wasmFileSize: 61222367,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
